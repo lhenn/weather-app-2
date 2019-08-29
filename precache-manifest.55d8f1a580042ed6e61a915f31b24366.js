6a39e8520fc6c212b482311129442cf167b4e849
Laura Henn <hennlaura1@gmail.com>
2019-08-29 12:10:31 +0200

    Updates

---
+++ precache-manifest.55d8f1a580042ed6e61a915f31b24366.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b57c26948e70e0c82175322404a9730",
+    "url": "/weather-app-2/index.html"
+  },
+  {
+    "revision": "9ae34fe343cdbe892405",
+    "url": "/weather-app-2/static/css/main.706ae44d.chunk.css"
+  },
+  {
+    "revision": "4e029b16120c0b7539f5",
+    "url": "/weather-app-2/static/js/2.9742547c.chunk.js"
+  },
+  {
+    "revision": "9ae34fe343cdbe892405",
+    "url": "/weather-app-2/static/js/main.9dae4aeb.chunk.js"
+  },
+  {
+    "revision": "cfb4140a5e307c283d12",
+    "url": "/weather-app-2/static/js/runtime~main.a5f77d04.js"
+  },
+  {
+    "revision": "038b3f0a2eb34dc9ef9a5b7e275478ad",
+    "url": "/weather-app-2/static/media/clear-night.038b3f0a.png"
+  },
+  {
+    "revision": "c031971784eddcc6ce3384125ad3c0ab",
+    "url": "/weather-app-2/static/media/clear.c0319717.png"
+  },
+  {
+    "revision": "71de5dc3012c09ebf248102a011dc577",
+    "url": "/weather-app-2/static/media/clouds-night.71de5dc3.png"
+  },
+  {
+    "revision": "513b857aa344f880af5f4f93197df989",
+    "url": "/weather-app-2/static/media/clouds.513b857a.png"
+  },
+  {
+    "revision": "0a0ada4247750c569b3f4ea6fb1bac47",
+    "url": "/weather-app-2/static/media/header.0a0ada42.png"
+  },
+  {
+    "revision": "e4d8aebcc71ae126874fef53ffdaf927",
+    "url": "/weather-app-2/static/media/rain.e4d8aebc.png"
+  }
+]);
